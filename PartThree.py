ca08622276994ed598604f53602dd1e144d2839c
ericnam <ericnam86@utexas.edu>
2016-05-09 17:22:18 -0500

    first commit

---
+++ PartThree.py
@@ -0,0 +1,18 @@
+class cars(object):
+    def f(self):
+        data = {
+            'brand': 'Nissan',
+            '$brand': lambda x: data.update({'name': x}),
+            'model': '350z',
+            '$model': lambda x: data.update({'model': x})
+        }
+
+        def cf (self, d):
+            if d in data:
+                return data[d]
+            else:
+                return None
+        return cf
+    run = f(1)
+
+
